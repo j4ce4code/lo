20d97ee5f9f050ffa6856d364e0e9b917e0ba021
jack <jack.giannini@gmail.com>
2024-08-28 21:16:53 -0400

    version 0.1.4

--- lo.go
+++ lo.go
@@ -3,5 +3,12 @@ package lo
 import "fmt"
 
 func PrintVersion() {
-	fmt.Println("0.1.3")
+	fmt.Println("0.1.4")
+}
+
+func Ternary(condition bool, this any, that any) any {
+	if condition {
+		return this
+	}
+	return that
 }
